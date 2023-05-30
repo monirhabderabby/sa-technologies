8c00bac3f4ccefec572e9f927bce6f17e755be56
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-05-30 17:44:24 +0600

    replaced; source path in config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
-    content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+    content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./Components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
     theme: {
         extend: {
             backgroundImage: {
