ae91a1e2c4d0b018dd954359c59ab45111ee8335
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2023-06-08 22:40:12 +0600

    added: contact section

--- Components/Shared/ContactForm/ContactFormContainer.js
+++ Components/Shared/ContactForm/ContactFormContainer.js
@@ -3,7 +3,7 @@ import { Input } from "@material-tailwind/react";
 import { PrimaryBtn } from "../buttons/PrimaryBtn";
 export const ContactFormContainer = () => {
     return (
-        <div className="h-[493px] w-[445px] bg-white shadow-[0px_4px_12px_rgba(12,68,204,0.1)] rounded-[6px] absolute top-[100px] right-[200px] py-[120px] px-[50px] flex flex-col gap-y-[20px]">
+        <div className="h-[493px] w-full lg:w-[445px] bg-white shadow-[0px_4px_12px_rgba(12,68,204,0.1)] rounded-[6px] absolute top-[300px] lg:top-[100px] right-[0px] lg:right-[150px] py-[120px] px-[50px] flex flex-col gap-y-[30px]">
             <Input variant="standard" label="Name" />
             <Input variant="standard" label="Email" />
             <Input variant="standard" label="Messasge" />
--- app/contact/page.js
+++ app/contact/page.js
@@ -2,11 +2,29 @@ import { ContactFormContainer } from "@/Components/Shared/ContactForm/ContactFor
 
 const Contact = () => {
     return (
-        <div className="h-full flex justify-center items-center relative">
-            <div className="w-full h-[400px] bg-orange-500">
-                <ContactFormContainer />
+        <>
+            <div className="h-full flex flex-col justify-center items-center relative">
+                <div className="w-full h-[400px] bg-orange-500">
+                    <ContactFormContainer />
+                </div>
             </div>
-        </div>
+            <section className="max-w-[1200px] mx-auto">
+                <div className=" flex flex-col lg:flex-row items-center gap-x-[60px] mt-[60px]">
+                    <div>
+                        <h2 className="text-[#292D33] font-inter font-semibold">Phone</h2>
+                        <p className="text-[#292D33] text-[14px] font-lato font-thin tracking-wider">01956306002</p>
+                    </div>
+                    <div>
+                        <h2 className="text-[#292D33] font-inter font-semibold">Email</h2>
+                        <p className="text-[#292D33] text-[14px] font-thin font-lato tracking-wider">monirhrabby.personal@gmail.com</p>
+                    </div>
+                    <div>
+                        <h2 className="text-[#292D33] font-inter font-semibold">Address</h2>
+                        <p className="text-[#292D33] text-[14px] font-lato font-thin tracking-wider">Morkun poschim para</p>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
